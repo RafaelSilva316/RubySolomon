038d107721922ec6234da4c66ed083969a7b674d
rafaelsilva316 <rafaelsilva316@gmail.com>
2021-12-05 21:51:36 -0500

    created resources page

--- app.js
+++ app.js
@@ -3,19 +3,34 @@ let headerEl = document.querySelector("header");
 let menuList = document.createElement("ul");
 // add about me resources 518-555-5555 and email us
 let about = document.createElement("li");
-about.innerHTML = "about me";
+let aboutA = document.createElement("a");
+aboutA.innerHTML = "about me";
+aboutA.setAttribute("href", "./about.html");
+about.appendChild(aboutA);
+
 let resources = document.createElement("li");
-resources.innerHTML = "resources";
+let resourcesA = document.createElement("a");
+resourcesA.innerHTML = "resources";
+resourcesA.setAttribute("href", "./resources.html");
+resources.appendChild(resourcesA);
+
 let phone = document.createElement("li");
-phone.innerHTML = "518-555-5555";
+let phoneA = document.createElement("a");
+phoneA.innerHTML = "518-555-5555";
+phoneA.setAttribute("href", "tel:518-555-5555");
+phone.appendChild(phoneA);
+
 let email = document.createElement("li");
-email.innerHTML = "email me";
+let emailA = document.createElement("a");
+emailA.innerHTML = "email me";
+emailA.setAttribute("href", "mailto:rafaelsilva316@gmail.com");
+email.appendChild(emailA);
 
 menuList.append(about, resources, phone, email);
 let hiddenNavList = menuList.childNodes;
 
 hiddenNavList.forEach(function (li) {
-  li.classList.add("cta-list-link");
+  li.querySelector("a").classList.add("cta-list-link");
 });
 
 let hiddenMenu = document.querySelector(".hidden-menu");
